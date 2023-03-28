bf78bcb0db249ed3596dbe9dc3e891bc64930eb2
katarzynachodyniecka <katarzyna.chodyniecka@gmail.com>
2023-03-28 14:33:10 +0200

    task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const galleryEl = document.querySelector(".gallery");
+let isVisible = false;
+let instance;
+function createGallery() {
+  for (let i = 0; i < 9; i++) {
+    const item = document.createElement("div");
+    item.classList.add("gallery__item");
+    const gallery = `<a class="gallery__link" href="${galleryItems[i].original}">
+<img
+  class="gallery__image"
+  src="${galleryItems[i].preview}"
+  data-source="${galleryItems[i].original}"
+  alt="${galleryItems[i].description}"
+/>
+</a>`;
+    item.innerHTML += gallery;
+    galleryEl.appendChild(item);
+  }
+}
+
+createGallery();
+galleryEl.addEventListener("click", (e) => {
+  e.preventDefault();
+  if (e.target.nodeName !== "IMG") return;
+  instance = basicLightbox.create(
+    `
+  <img src="${e.target.dataset.source}"/>
+  `
+  );
+  instance.show();
+  isVisible = true;
+});
+window.addEventListener("keyup", (e) => {
+  if (!isVisible) return;
+  if (e.key === "Escape") {
+    instance.close();
+    isVisible = false;
+  }
+});
